df77dfbd7f53d2bdf2d2875360113286969d215a
Filip Jakubowski <surokraci@outlook.com>
2021-12-22 09:45:59 +0100

    form skonczony

--- Sprawdzian1/kolokwium-fj/src/Gifts/GiftForm.js
+++ Sprawdzian1/kolokwium-fj/src/Gifts/GiftForm.js
@@ -1,14 +1,14 @@
 import { Field, Form, Formik } from "formik"
 import { useEffect } from "react";
 import { connect } from 'react-redux';
-import {v4 as uuidv4 } from 'uuid';
 import { addGift } from "../actions/GiftAction";
 import { withRouter } from "react-router-dom";
 
 
-const GiftForm = ({addGift, gifts}, props) => {
+const GiftForm = ({history, addGift, gifts}, props) => {
     const handleSubmit = (values) =>{
         addGift(values)
+        history.push('/')
     }
 
 
@@ -18,12 +18,45 @@ const GiftForm = ({addGift, gifts}, props) => {
             <Formik 
                 initialValues={{
                     name: "",
-                    gift: ""
+                    colour: "",
+                    model: "",
+                    v2:false,
+                    size:""
+
                 }}
                 onSubmit = {(values) => handleSubmit(values)}>
                     <Form>
-                        <Field name="name"/>
-                        <Field name="date"/>
+                        <Field name="name" placeholder="Imie i Nazwisko"/>
+                        <Field name="colour" placeholder="kolorystyka"/>
+                        <Field  name="model" as="select">
+                        <option value="Yeezy Boost 350"> Yeezy Boost 350 </option>  
+                        <option value="Yeezy 450"> Yeezy 450 </option>
+                        <option value="Yeezy 500"> Yeezy 500 </option>
+                        <option value="Yeezy Boost 700"> Yeezy Boost 700 </option>  
+                        </Field>
+
+                        <label>
+                            <Field type="checkbox" name="v2" />
+                            Model V2
+                        </label>
+                        <div role="group" aria-labelledby="my-radio-group">
+                            <label>
+                            <Field type="radio" name="size" value="40" />
+                            40
+                            </label>
+                            <label>
+                            <Field type="radio" name="size" value="41" />
+                            41
+                            </label>
+                            <label>
+                            <Field type="radio" name="size" value="42" />
+                            42
+                            </label>
+                            <label>
+                            <Field type="radio" name="size" value="43" />
+                            43
+                            </label>
+                        </div>
                         <button type="submit">
                             Zatwierdz
                         </button>
