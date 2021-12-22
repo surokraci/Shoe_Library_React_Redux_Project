b175fc794bfe9440945bf74bc57fb341121cf7f8
Filip Jakubowski <surokraci@outlook.com>
2021-12-22 09:08:44 +0100

    dodanie funckcji listy

--- Sprawdzian1/kolokwium-fj/src/Gifts/GiftForm.js
+++ Sprawdzian1/kolokwium-fj/src/Gifts/GiftForm.js
@@ -3,9 +3,36 @@ import { useEffect } from "react";
 import { connect } from 'react-redux';
 import {v4 as uuidv4 } from 'uuid';
 import { addGift } from "../actions/GiftAction";
+import { withRouter } from "react-router";
 
 
-const GiftForm = (props) => {
+const GiftForm = ({addGift, gifts}, props) => {
+    const handleSubmit = (values) =>{
+        addGift(values)
+    }
+
+
+    return (
+        <div>
+            <h3>Rzeczy</h3>
+            <Formik 
+                initialValues={{
+                    name: "",
+                    gift: ""
+                }}
+                onSubmit = {(values) => handleSubmit(values)}>
+                    <Form>
+                        <Field name="name"/>
+                        <Field name="date"/>
+                        <button type="submit">
+                            Zatwierdz
+                        </button>
+                    </Form>
+                </Formik>
+        </div>
+    )
+
+
     
 
    
---
+++ Sprawdzian1/kolokwium-fj/src/Gifts/GiftList.js
@@ -0,0 +1,53 @@
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { getProductList } from "../actions/ProductAction";
+import { Link } from "react-router-dom";
+import { deleteGift } from "../actions/GiftAction";
+
+const GiftList = ({ gifts, deleteGift } ,props) => {
+    const handleClick = (values) => {
+        console.log("usunieto prezent");
+        deleteGift(values);
+        
+    }
+
+
+    return (
+        <div>
+            <h3>Produkty</h3>
+            
+            {
+                gifts.map(gift => {
+                    return (
+                    <div>
+                        <Link to={`gifts/${gift.name}`}>{gift.name}</Link>
+                        <div>
+                            <button onClick={() => 
+                                handleClick(product.id)}>Usuń</button>
+                        </div>
+                    </div>)})
+            }
+            <div>
+                <Link to="/giftForm">
+                    <button type="button">
+                        Dodaj nowy prezent
+                    </button>
+                </Link>
+            </div>
+            
+        </div>
+    )
+};
+
+const mapStateToProps = (state) => {
+    return {
+        gifts: state.gifts
+    };
+}
+
+const mapDispatchToProps = {
+    deleteGift
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductList);
