aa4c8891321a4eeafe9dd5492d3789bbec0e722b
surokraci <75081441+surokraci@users.noreply.github.com>
2022-04-03 23:14:02 +0200

    Delete LAB04/lab04 directory

--- LAB04/lab04/src/actions/DirectorAction.js
+++
@@ -1,9 +0,0 @@
-export const addDirectorAction = (payload) =>({
-    type: 'DIRECTOR_ADD',
-    payload
-})
-
-export const editDirectorAction = (payload) =>({
-    type: 'DIRECTOR_EDIT',
-    payload
-})
